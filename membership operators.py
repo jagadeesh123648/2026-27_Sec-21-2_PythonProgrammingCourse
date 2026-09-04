6b19c9a8d2c234e1f5a13ab2cf6b74ace0c73b78
jagadeesh123648 <arigijagadeesh@gmail.com>
2026-09-04 14:37:47 +0530

    membership operators

---
+++ membership operators.py
@@ -0,0 +1,34 @@
+#in membership operators
+a=[1,2,3,4,5]
+b=3
+result = b in a
+print("result of",b,"in",a,"is:",result)
+
+#not in membership operators
+a=[2,4,6,8]
+b=3
+result =b in a
+print("result of",b,"in",a,"is:",result)
+
+#in membership operators
+a=[1,3,5,7]
+b=3
+result =b in a
+print("result of",b,"in",a,"is:",result)
+#not in membership operators
+a=[1,2,3,4,5]
+b=8
+result =b in a
+print("result of",b,"in",a,"is:",result)
+
+#in memberdship operators
+a=[1,2,3,4,5]
+b=4
+result =b in a
+print("result of",b,"in",a,"is:",result)
+
+#not in membership operators
+a=[1,2,3,4,5]
+b=9
+result =b in a
+print("result of",b,"in",a,"is:",result)

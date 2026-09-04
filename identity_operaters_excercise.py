0b41b90c27f5ff327575f267033f4b79a715f954
jagadeesh123648 <arigijagadeesh@gmail.com>
2026-09-04 14:21:25 +0530

    identity_operators

---
+++ identity_operaters_excercise.py
@@ -0,0 +1,14 @@
+a=[1,2,3]
+b=a
+result =b is a
+print("result of b is a is : ",result)
+
+c=[1,2,3]
+d=[1,2,3]
+result = c is d
+print("result of c is d is : ",result)
+
+a=[23,34,45]
+b=[23,34,45]
+result = a is b
+print("result of a is b is : ",result)

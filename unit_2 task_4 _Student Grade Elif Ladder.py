05f5e7912bc14ff9e91da1fa32fca76defa183d6
jagadeesh123648 <arigijagadeesh@gmail.com>
2026-09-25 16:15:50 +0530

    unit_2 task_4

---
+++ unit_2 task_4 _Student Grade Elif Ladder.py
@@ -0,0 +1,13 @@
+# input students grade elif ladder
+marks=int(input("Enter marks out of 100:"))
+if marks>=90:
+    print(f"grade:A")
+elif marks >80:
+    print(f"grade:B")
+elif marks>70:
+    print(f"grade:C")
+elif marks<70:
+    print(f"grade:D")
+
+
+

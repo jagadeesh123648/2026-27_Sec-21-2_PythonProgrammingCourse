93cd275024eff6e099c7354c3cbd4d329c1884c6
jagadeesh123648 <arigijagadeesh@gmail.com>
2026-09-25 15:10:41 +0530

    unit_2 task_3

---
+++ unit_2 task_3 Positive, negative, or zero.py
@@ -0,0 +1,8 @@
+# input check for number is positive,negative,or zero
+num=int(input("Enter the num:"))
+if num>0:
+    print(f"the number is positive:")
+elif num<0:
+    print(f"the number is negative:")
+else:
+    print(f"the number is zero:")
